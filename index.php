33c599c97b5f0a720589e22dd3affe9a2e32ee04
simgortz <business@simgortz.com>
2019-09-30 13:30:03 +0200

    First commit
    Commiting before I do more work just to test this GitHub thing

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style/reset.css">
+        <link rel="stylesheet" type="text/css" href="style/stylesheet.css">
+        <title>Simon Gørtz</title>
+    </head>
+    <body>
+        test
+    </body>
+</html>
