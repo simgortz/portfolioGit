567ed264c03b0a42a82fed20b75528b5aeb911b4
simgortz <business@simgortz.com>
2019-10-02 12:29:06 +0200

    added content to aboutme.php
    added background, pic and short introduction - and first responsive design test

--- subpages/aboutme.php
+++ subpages/aboutme.php
@@ -1,1 +1,7 @@
-aboutme php
+<div id="aboutMe">
+    <div id="profileText">
+        <h1>Simon Gørtz</h1>
+        <p>Hello! My name is Simon Gørtz. I am currently studying Multimedia Design at the Copenhagen Business Academy with dreams of one day utilizing my innovative thinking to design User Interfaces and Experiences for video games.</p>
+    </div>
+    <img src="media/imgs/simon.jpg">
+</div>
