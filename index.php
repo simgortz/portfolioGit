ed2ddcf529008fc8ab2934f022183714df5753d1
simgortz <business@simgortz.com>
2019-10-10 09:43:17 +0200

    Updated responsive design stuff
    Added meta tag, changed menu on mobile slightly

--- index.php
+++ index.php
@@ -17,9 +17,11 @@ ini_set('include_path', ROOT . '/subpages/');
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="style/reset.css">
         <link rel="stylesheet" type="text/css" href="style/stylesheet.css">
         <link rel="stylesheet" href="https://use.typekit.net/cbx2oqw.css">
+        <link rel="icon" href="media/icons/icon64.png">
         <title>Simon Gørtz<?php
             if($page == 'studywork'){
                 echo ' - Study Work';
